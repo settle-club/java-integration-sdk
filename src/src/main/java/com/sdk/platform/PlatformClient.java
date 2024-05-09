320d142a79644f32e528964ff312030d00651f05
sharansalian <sharansalian.dev@gmail.com>
2024-05-09 22:04:56 +0530

    Remove application client

--- src/src/main/java/com/sdk/platform/PlatformClient.java
+++ src/src/main/java/com/sdk/platform/PlatformClient.java
@@ -29,30 +29,4 @@ public class PlatformClient {
         
     }
 
-    public ApplicationClient application(String applicationId) {
-        return new ApplicationClient(config, applicationId);
-    }
-
-    @Getter
-    public class ApplicationClient{
-
-        
-        public PlatformService.CustomerService.ApplicationClient customer;
-        
-        public PlatformService.CreditService.ApplicationClient credit;
-        
-        public PlatformService.MultiKycService.ApplicationClient multiKyc;
-        
-
-        public ApplicationClient(PlatformConfig platformConfig, String applicationId) {
-            
-            this.customer = new PlatformService.CustomerService(platformConfig).new ApplicationClient(platformConfig, applicationId);
-            
-            this.credit = new PlatformService.CreditService(platformConfig).new ApplicationClient(platformConfig, applicationId);
-            
-            this.multiKyc = new PlatformService.MultiKycService(platformConfig).new ApplicationClient(platformConfig, applicationId);
-            
-        }
-
-    }
 }
