1b3cb206250a3d568701ea9d5a570bfc9bf34035
Nikhil Manapure <nikhilmshchs@gmail.com>
2024-11-18 05:59:07 +0000

    [Auto Generated] v1.0

--- src/main/java/com/sdk/platform/PlatformOauthClient.java
+++ src/main/java/com/sdk/platform/PlatformOauthClient.java
@@ -114,7 +114,7 @@ public class PlatformOauthClient {
                 + "&redirect_uri=" + redirectUri + "&state=" + state + "&access_mode=" + accessMode
                 + "&response_type=code";
 
-        var queryString = config.getDomain() + URI + config.getApiKey() + "/authorize?" + query;
+        String queryString = config.getDomain() + URI + config.getApiKey() + "/authorize?" + query;
         Request request = new Request.Builder().url(queryString)
                 .get()
                 .build();
